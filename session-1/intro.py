8bc76747021ef7db13bbe9cb4a6bdb4324b8961e
mbreiter <matthew.reiter1@hotmail.com>
2016-07-23 15:21:02 -0400

    adding intro program

---
+++ session-1/intro.py
@@ -0,0 +1,11 @@
+print("Hello, world!")
+print(2.7)
+
+number = 4
+word = "star"
+
+print(type(number))
+
+favoriteMusic = raw_input("Enter your favourite type of music: ")
+
+print("My favorite is " + favoriteMusic)
