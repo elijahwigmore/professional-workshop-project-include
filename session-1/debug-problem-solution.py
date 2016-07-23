6ed02cf014d42d4ac82599dabd8b657d246d0d1b
Bryon Leung <teslaslegacy@gmail.com>
2016-07-23 17:17:51 -0400

    Added solution to debug problem

---
+++ session-1/debug-problem-solution.py
@@ -0,0 +1,31 @@
+import math
+
+#We're using rounded g = 10 m/s^2.
+g = -10
+
+cannon_speed = float(raw_input("Enter velocity of cannonball: ")) 
+
+
+#Fire the cannon at a 45 degree angle. This means both x and y velocity
+#are half of the speed of the cannonball.
+cannonball_velocity_x = cannon_speed/2
+cannonball_velocity_y = cannon_speed/2
+
+a = g/2
+b = cannonball_velocity_y
+
+
+#Impact time = vel*cos(cannon_angle) * (2*vel*sin(cannon_angle)/g)
+#Here, this simplifies down to -(yvel / g), assuming g is negative since
+#it signifies downward acceleration.
+time_to_impact = -(2*b)/(2*a)
+impact_x = cannonball_velocity_x * time_to_impact
+
+
+if (impact_x >= 0 ):
+    greater_less = "right"
+else:
+    greater_less = "left"
+
+#The printed impact point should not round the result to the nearest whole number.
+print "Your shot is " + str(math.fabs(impact_x)) + " to the " + greater_less + " of the cannon."
